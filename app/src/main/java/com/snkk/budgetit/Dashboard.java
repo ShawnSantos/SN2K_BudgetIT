f33db0a77e6451775132160027e1b560d7768984
MysterRamen <karen.elemia@gmail.com>
2020-11-27 23:24:55 +0800

    initial commit with bottom nav

--- app/src/main/java/com/snkk/budgetit/Dashboard.java
+++ app/src/main/java/com/snkk/budgetit/Dashboard.java
@@ -12,51 +12,16 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Dashboard extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        bottomNavigationView.findViewById(R.id.bottomNav);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container,new DashboardFragment()).commit();
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new BottomNavigationView.OnNavigationItemSelectedListener()
-    {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            
-            Fragment fragment = null;
 
-            switch (menuItem.getItemId())
-            {
-                case R.id.dashboard:
-                    fragment=new DashboardFragment();
-                    break;
-
-                case R.id.stats:
-                    fragment=new StatsFragment();
-                    break;
-
-                case R.id.add_trans:
-                    fragment=new AddTransFragment();
-                    break;
-
-                case R.id.categories:
-                    fragment=new CategoriesFragment();
-                    break;
-
-                case R.id.forum:
-                    fragment=new ForumFragment();
-                    break;
-            }
-            getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
-            return true;
-        }
-    };
 
 }
--- app/src/main/java/com/snkk/budgetit/RegisterActivity.java
+++ app/src/main/java/com/snkk/budgetit/RegisterActivity.java
@@ -87,3 +87,5 @@ public class RegisterActivity extends AppCompatActivity {
         startActivity(i);
     }
 }
+
+
