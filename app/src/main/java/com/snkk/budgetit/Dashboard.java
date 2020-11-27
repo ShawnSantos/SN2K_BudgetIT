b02283e4c3ad1536476ed29c14ef8a0b83752291
ndzjmn <jamena.nadine@gmail.com>
2020-11-27 20:20:20 +0800

    added bottom nav bar

--- app/src/main/java/com/snkk/budgetit/Dashboard.java
+++ app/src/main/java/com/snkk/budgetit/Dashboard.java
@@ -1,14 +1,62 @@
 package com.snkk.budgetit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Dashboard extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        bottomNavigationView.findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new DashboardFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new BottomNavigationView.OnNavigationItemSelectedListener()
+    {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            
+            Fragment fragment = null;
+
+            switch (menuItem.getItemId())
+            {
+                case R.id.dashboard:
+                    fragment=new DashboardFragment();
+                    break;
+
+                case R.id.stats:
+                    fragment=new StatsFragment();
+                    break;
+
+                case R.id.add_trans:
+                    fragment=new AddTransFragment();
+                    break;
+
+                case R.id.categories:
+                    fragment=new CategoriesFragment();
+                    break;
+
+                case R.id.forum:
+                    fragment=new ForumFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+            return true;
+        }
+    };
+
 }
