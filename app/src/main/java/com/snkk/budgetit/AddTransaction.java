b171fa261c139dd59905da20b43c28872d89fe0f
ShawnSantos <shawnaustin09@outlook.com>
2020-11-29 01:54:09 +0800

    added input amount functionality

--- app/src/main/java/com/snkk/budgetit/AddTransaction.java
+++ app/src/main/java/com/snkk/budgetit/AddTransaction.java
@@ -1,9 +1,12 @@
 package com.snkk.budgetit;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,17 +14,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class AddTransaction extends Fragment {
-
-    private TextView textViewAmount;
+    TextView textViewAmount;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.activity_add_transaction,container,false);
 
         textViewAmount = (TextView) view.findViewById(R.id.textView_amount);
         textViewAmount.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 openDialog();
@@ -32,7 +34,36 @@ public class AddTransaction extends Fragment {
     }
 
     public void openDialog() {
-        AmountDialog amountDialog = new AmountDialog();
-        amountDialog.show(getActivity().getSupportFragmentManager(), "amount dialog");
+
+        final AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+        View mView = getLayoutInflater().inflate(R.layout.layout_dialog,null);
+
+        final EditText editTextInputAmount = (EditText) mView.findViewById(R.id.input_amount);
+        Button btn_cancel = (Button) mView.findViewById(R.id.btn_cancel);
+        Button btn_okay = (Button) mView.findViewById(R.id.btn_okay);
+
+        alert.setView(mView);
+
+        final AlertDialog alertDialog = alert.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                alertDialog.dismiss();
+            }
+        });
+
+        btn_okay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                textViewAmount.setText(editTextInputAmount.getText().toString());
+                alertDialog.dismiss();
+            }
+        });
+
+        alertDialog.show();
     }
 }
