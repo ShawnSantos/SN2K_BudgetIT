96c3ae6fa5928523541363b6d0b8268bdf6f682e
MysterRamen <karen.elemia@gmail.com>
2020-11-28 18:59:21 +0800

    dash

--- app/src/main/java/com/snkk/budgetit/Dashboard.java
+++ app/src/main/java/com/snkk/budgetit/Dashboard.java
@@ -7,12 +7,25 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.snkk.budgetit.Models.User;
+
+import java.util.EventListener;
 
 public class Dashboard extends AppCompatActivity {
 
-
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +33,29 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        uid = user.getUid();
+
+        final TextView nameTextView = (TextView) findViewById(R.id.name);
+
+        reference.child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null){
+                    String name = userProfile.getName();
+
+                    nameTextView.setText(name + "!");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
-
-
-
 }
