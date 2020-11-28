cbc49a624e516e01587cff62174b83d386fc17b2
ShawnSantos <shawnaustin09@outlook.com>
2020-11-28 20:19:35 +0800

    Added add transaction feature

--- app/src/main/java/com/snkk/budgetit/AddTransaction.java
+++ app/src/main/java/com/snkk/budgetit/AddTransaction.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +12,27 @@ import androidx.fragment.app.Fragment;
 
 public class AddTransaction extends Fragment {
 
+    private TextView textViewAmount;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_add_transaction,container,false);
+
+        View view = inflater.inflate(R.layout.activity_add_transaction,container,false);
+
+        textViewAmount = (TextView) view.findViewById(R.id.textView_amount);
+        textViewAmount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+
+        return view;
+    }
+
+    public void openDialog() {
+        AmountDialog amountDialog = new AmountDialog();
+        amountDialog.show(getActivity().getSupportFragmentManager(), "amount dialog");
     }
 }
---
+++ app/src/main/java/com/snkk/budgetit/AmountDialog.java
@@ -0,0 +1,46 @@
+package com.snkk.budgetit;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class AmountDialog extends AppCompatDialogFragment {
+
+    private EditText editTextAmount;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog, null);
+
+        builder.setView(view)
+                .setTitle("Enter Amount")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        editTextAmount.findViewById(R.id.input_amount);
+
+        return builder.create();
+    }
+}
